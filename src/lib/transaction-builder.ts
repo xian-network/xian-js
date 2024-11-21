bc9dd8346b4b0f817a46f356d3aa13702af3679f
Michael Buritica <94705606+Revolutionnnn@users.noreply.github.com>
2024-11-21 09:17:07 -0500

    Update transaction-builder.ts

--- src/lib/transaction-builder.ts
+++ src/lib/transaction-builder.ts
@@ -99,7 +99,6 @@ export class TransactionBuilder {
 			return result;
 		}
 		catch (error) {
-			console.log(error)
 			throw error;
 		}
 	}
@@ -121,7 +120,6 @@ export class TransactionBuilder {
 			
 			return await this.masternodeApi.simulateTxn(tx);
 		} catch (error) {
-			console.log(error)
 			throw error;
 		}
 	}
